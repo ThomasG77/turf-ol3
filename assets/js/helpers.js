21b8586a57bed213fef80fe04270835545fe6d68
ThomasG77 <thomas_gratier@yahoo.fr>
2016-09-28 02:28:24 +0200

    Remove fetchJSON from samples and from helpers.js as it was unuseful with shorter fetch call to JSON

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -7,16 +7,3 @@ var geojsonToFeatures = function geojsonToFeatures(fc, options) {
   var features = format.readFeatures(fc, options);
   return features;
 };
-
-// Playground using fetch API
-// For the demo purpose, we don't want to use OpenLayers 3
-// to make call to geojson files here
-var fetchJSON = function(url) {
-  return fetch(url).then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      return json;
-    }).catch(function(ex) {
-      console.log('parsing failed', ex);
-    });
-};
